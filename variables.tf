8f66556dbdb19bd94ca30f80e2bd7bd846f3ca00
Samuel Chai <samuel.c@investing.com>
2023-11-02 12:06:49 +0200

    this

--- variables.tf
+++ variables.tf
@@ -126,7 +126,7 @@ variable "cloudwatch-alarm-arn" {
   type    = string
   default = null
   validation {
-    condition = var.cloudwatch-alarm-arn == null || startswith(var.cloudwatch-alarm-arn, "arn")
+    condition = var.cloudwatch-alarm-arn == null || try(startswith(var.cloudwatch-alarm-arn, "arn"), false)
     error_message = "The arn must be a full arn, not a name or an id."
   }
 }
